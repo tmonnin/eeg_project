7e8ae6eecdb7488ec960bb4d7de1c89ec7d5539b
Thomas Monninger <t.monninger@t-online.de>
2021-03-18 23:12:57 +0100

    ICA with highpass in config

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ fname.add('annotations_dir', '{path}/annotations')
 fname.add('filter', '{subject_dir}/filter-{fmin}-{fmax}.fif')
 fname.add('cleanchannels', '{subject_dir}/cleanchannels-{bad_channels}.fif')
 fname.add('cleansegments', '{subject_dir}/cleansegments-{strategy}.fif')
-fname.add('ica', '{subject_dir}/ica.fif')
+fname.add('ica', '{subject_dir}/ica-{fmin}.fif')
 fname.add('reference', '{subject_dir}/reference.fif')
 
 
