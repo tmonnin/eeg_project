b4d7023b234bc2ef892d527bf80dee1c77e17c4f
Thomas Monninger <t.monninger@t-online.de>
2021-03-26 12:46:59 +0100

    Refine re-referencing

--- _04_reference.py
+++ _04_reference.py
@@ -13,10 +13,18 @@ class Reference(Base):
         # Literature suggests "Average" as new reference for N170 task 
         # https://www.sciencedirect.com/science/article/pii/S1053811920309502
         # "For analysis of the N170, the EEG signals were referenced to the average of all 33 sites (because the average reference is standard in the N170 literature)."
+        # Choose projection=True to ensure that reference is correctly set even if channel choice is changed later on
+        # "it is strongly recommended to use the average-reference-as-projection approach"
+        # https://mne.tools/stable/auto_tutorials/preprocessing/plot_55_setting_eeg_reference.html
+        # Terminal output shows warning: "Average reference projection was added, but has not been applied yet."
+        # "By default, raw.plot() will apply the projectors in the background before plotting"
+        # https://mne.tools/dev/auto_tutorials/preprocessing/plot_45_projectors_background.html
         self.raw.load_data()
-        mne.set_eeg_reference(self.raw, ref_channels='average', copy=False, projection=False, ch_type='auto', forward=None, verbose=None)
-        figure_after_reference = self.raw.plot(n_channels=len(self.raw.ch_names), show=False)#,scalings =40e-6)
-        self.add_figure(figure=figure_after_reference, caption="After EEG is re-referenced", section="Preprocessing")
+        mne.set_eeg_reference(self.raw, ref_channels='average', copy=False, projection=True, ch_type='auto', forward=None, verbose=None)
+        figure_without_reference = self.raw.plot(proj=False, n_channels=len(self.raw.ch_names), show=False)#,scalings =40e-6)
+        self.add_figure(figure=figure_without_reference, caption="EEG without re-referencing", section="Preprocessing")
+        figure_with_reference = self.raw.plot(proj=True, n_channels=len(self.raw.ch_names), show=False)#,scalings =40e-6)
+        self.add_figure(figure=figure_with_reference, caption="EEG with re-referencing", section="Preprocessing")
 
 
 if __name__ == '__main__':
