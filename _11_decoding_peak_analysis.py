549deff0fda079925f3f60c082003b63b5a7e654
Thomas Monninger <t.monninger@t-online.de>
2021-03-29 10:26:07 +0200

    Add significance plot to decoding peak analysis

--- _11_decoding_peak_analysis.py
+++ _11_decoding_peak_analysis.py
@@ -1,6 +1,6 @@
 import json
 import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib import ticker, pyplot as plt
 import scipy
 import mne
 from config import fname
@@ -17,11 +17,16 @@ class DecodingPeakAnalysis(Base):
         # Decoding analysis Decode the main contrast of the experiment across time
         # RQ: When is information about the conditions in our data available?
         # https://mne.tools/stable/auto_tutorials/machine-learning/plot_sensors_decoding.html
-        test_results = []
+        pvalues = []
+        score_mean = []
+        time_lst = []
+        time_lst_plt = []
         for start_time in np.arange(self.config["t_start"], self.config["t_end"], self.config["t_sampling"]):
             #peaks = []
+            time_lst += [start_time]
+            time_lst_plt += [start_time, start_time + self.config["t_sampling"]]
             data = []
-            for self.subject in self.config["subjects"][0:15]:
+            for self.subject in self.config["subjects"]:#[0:15]:
                 with open(fname.decodingpeak(subject=self.subject), "r") as json_file:
                     json_data = json.load(json_file)
                     json_data = json_data["StandardScaler-LogisticRegression"]
@@ -29,9 +34,9 @@ class DecodingPeakAnalysis(Base):
                     time_idx = [i for i, x in enumerate(times) if x >= start_time and x < start_time + self.config["t_sampling"]]
                     score = np.mean(scores[time_idx[0]:time_idx[-1]+1])
                     #peaks.append([peak["peak_time"], peak["peak_alt"]])
-                    data.append([score, 0.5])
+                    data.append(score)
+            score_mean += [np.mean(data)] #* 2  # plateaus for plotting
             #peaks = np.array(peaks)
-            data = np.array(data)
             #plt.hist(peaks[:,0])
             #plt.hist(data)
             #plt.show()
@@ -39,16 +44,44 @@ class DecodingPeakAnalysis(Base):
             # Non-parametric cluster-level paired t-test
             # The first dimension should correspond to the difference between paired samples (observations) in two conditions.
             # https://mne.tools/dev/generated/mne.stats.permutation_cluster_1samp_test.html
-            alpha = 0.05
-            statistics, p_value = scipy.stats.ttest_1samp(data[:,0], 0.5, alternative='greater')
-            test_results.append([start_time, p_value])
+            alpha = 0.025
+            statistics, pvalue = scipy.stats.ttest_1samp(data, 0.5, alternative='greater')
+            pvalues.append(pvalue)
             # TODO effect size
         #word = "not "*(p_value >= alpha)
         #print(f"Difference of ERP peak between face and car condition is {word}significant with alpha={alpha} and p-value={p_value}.")
         #print(test_results)
-        test_results = np.array(test_results)
-        plt.plot(test_results[:,0], test_results[:,1])
-        plt.axhline(y=0.05, color='r', linestyle='-')
+        pvalues = np.array(pvalues)
+        fig, (ax1, ax2, ax3) = plt.subplots(3, 1, constrained_layout=True, gridspec_kw={'height_ratios': [4, 6, 1]}, figsize=(14, 8))
+        ax1.plot(time_lst_plt, np.repeat(score_mean, 2), label="score")
+        ax1.axhline(y=0.5, color="lightcoral", linestyle='--', label="chance")  # Horizontal line indicating chance (50%)
+        ax1.set_xlabel("Time [s]")
+        ax1.set_ylabel("ROC AUC Score")
+        ax1.set_xlim([0.0, 1.0])
+        ax1.set_title(f"Average decoding score across time")
+        ax1.legend()
+        ax1.grid(linestyle="dashed")
+        ax1.set_axisbelow(True)
+        ax1.xaxis.set_major_locator(ticker.MultipleLocator(0.1))
+
+        ax2.plot(time_lst_plt, np.repeat(pvalues, 2)*100, label="P-values")
+        ax2.axhline(y=alpha*100, color="lightcoral", linestyle='--', label=f"alpha={alpha*100}%")  # Horizontal line indicating chance (50%)
+        ax2.set_xlabel("Time [s]")
+        ax2.set_ylabel("%")
+        ax2.set_xlim([0.0, 1.0])
+        ax2.set_ylim([0.0*100, 1.0*100])
+        ax2.set_title(f"P-value of t-test across time")
+        ax2.legend()
+        ax2.grid(linestyle="dashed")
+        ax2.set_axisbelow(True)
+        ax2.xaxis.set_major_locator(ticker.MultipleLocator(0.1))
+        ax2.yaxis.set_major_locator(ticker.MultipleLocator(0.05*100))
+
+        ax3.pcolormesh(time_lst, [0,1], [pvalues < alpha], cmap="RdYlGn")
+        ax3.set_xlim([0.0, 1.0])
+        ax3.yaxis.set_visible(False)
+        ax3.set_title(f"Significance of t-test across time")
+
         plt.show()
 if __name__ == '__main__':
     DecodingPeakAnalysis().run()
