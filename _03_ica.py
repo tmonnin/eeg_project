0bf1af10166a996d6e6056683f6b50c5c4cde79e
Thomas Monninger <t.monninger@t-online.de>
2021-03-31 16:43:20 +0200

    Rework ICA code for manual cleaning

--- _03_ica.py
+++ _03_ica.py
@@ -1,5 +1,8 @@
+import os
+import numpy as np
 import mne
 import utils
+from config import fname
 from base import Base
 from _02_clean_segments import CleanSegments
 
@@ -10,9 +13,7 @@ class ICA(Base):
         super().__init__(self.__class__.__name__.lower(), prev, section=("Preprocessing", "ICA"))
 
     def process(self):
-        # TODO compare pre and post removing bad components with ICA
-        figure_pre_ica = self.raw.plot(show=False)
-        #self.add_figure(figure=figure_pre_ica, caption="Before applying ICA")
+        # Copy the raw object before altering due to ICA
         raw_pre_ica = self.raw.copy()
         raw_ica = self.raw.copy()
         raw_ica.load_data()
@@ -28,23 +29,30 @@ class ICA(Base):
             #- Lower passband edge: 2.00
             #- Lower transition bandwidth: 2.00 Hz (-6 dB cutoff frequency: 1.00 Hz)
             #- Filter length: 1691 samples (1.651 sec)
-        ica_interactive = False
-        if ica_interactive:        
+
+        if self.subject in self.config["subjects_preprocess"]:
+            # Compute and fit ICA
             ica = mne.preprocessing.ICA(method="fastica")
             ica.fit(raw_ica, verbose=True)
-            ica.plot_components(range(10))
-            # TODO check ica.detect_artifacts()
-            #ica.plot_properties(raw_ica,picks=[0,1],psd_args={'fmax': 35.},reject=None)
-            # Estimate sources given the unmixing matrix.
-            #icaact = ica.get_sources(raw_ica)
-            #plt.plot(icaact[5,0:20000][0].T)
-            #ica.plot_overlay(raw_ica,exclude=[1,8,9])
 
-        #### Use preprocessed data
-        ica, bad_comps = utils.load_ica(self.task, self.subject)
-        ica = utils.ica_info(self.raw, ica)
+            ica_interactive = False
+            if ica_interactive:
+                ica.plot_components(range(15), show=True)
+                #ica.detect_artifacts()
+                ica.plot_properties(raw_ica, picks=range(15), psd_args={'fmax': 35.}, reject=None, show=True)
+
+            # Subject is one of the three ones that are cleaned manually
+            path_badica = os.path.join(fname.annotations_dir, f"sub-{self.subject}_task-{self.task}_badica.txt")
+            with open(path_badica, "r") as f:
+                line = f.readline()
+                bad_comps = line.split(",")
+                bad_comps = np.array(bad_comps).astype(np.float)
+        else:
+            # Subject is not one of the manually cleaned ones, use precomputed ica
+            ica, bad_comps = utils.load_ica(self.task, self.subject)
+            ica = utils.ica_info(self.raw, ica)
+
         #ica.plot_properties(self.raw)
-        # TODO check how preprocessed ICA matches my preprocessed raw object
         # https://mne.tools/stable/generated/mne.preprocessing.ICA.html#mne.preprocessing.ICA.get_sources
         #Remove selected components from the signal.
         self.raw.load_data()
@@ -52,8 +60,6 @@ class ICA(Base):
         # ica.apply() works in-place and returns the altered object, doing both here
         self.raw = ica.apply(self.raw, exclude=bad_comps)
 
-        figure_post_ica = self.raw.plot(show=False)
-        #self.add_figure(figure=figure_post_ica, caption="After applying ICA")
         raw_pre_ica.load_data()
         figure_ica_overlay = ica.plot_overlay(raw_pre_ica, exclude=bad_comps, show=False)
         self.add_figure(figure=figure_ica_overlay, caption="ICA Overlay, ordinate in [V]")
